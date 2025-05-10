c2b20ff482a625311feb2cd6ccac70466683a181
Xi Yu <xiyu@Xis-MacBook-Pro.local>
2025-05-10 14:22:28 +0800

    Add Github Page configs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isGithubPages = process.env.GITHUB_PAGES === 'true';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: isGithubPages ? '/timeloved' : '',
+  assetPrefix: isGithubPages ? '/timeloved' : '',
 };
 
 export default nextConfig;
