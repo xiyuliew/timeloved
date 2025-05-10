de48b053bd9c3dad36fc325fe57dc230f5d10b7a
Xi Yu <xiyu@Xis-MacBook-Pro.local>
2025-05-10 16:16:58 +0800

    Alter config

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
 /** @type {import('next').NextConfig} */
 
-
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: isProd ? '/timeloved' : '',
-  assetPrefix: isProd ? '/timeloved' : '',
 };
 
 export default nextConfig;
